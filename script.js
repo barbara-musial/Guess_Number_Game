07e3b96232516bb53da79b96553132a7bc64e744
barbara-musial <basia.musial22@op.pl>
2021-10-28 16:55:33 +0200

    Update

--- script.js
+++ script.js
@@ -3,7 +3,6 @@
 let secretNumber = Math.floor(Math.random() * 20 + 1);
 let score = 20;
 let highscore = 0;
-console.log(secretNumber);
 
 document.querySelector("#check").addEventListener("click", function () {
   const usersInput = Number(document.querySelector("#usersInput").value);
@@ -13,13 +12,37 @@ document.querySelector("#check").addEventListener("click", function () {
       "You didn't choose any number!";
   } else if (usersInput === secretNumber) {
     document.querySelector("#hint").textContent = "You WON!";
+    document.querySelector("#secretNumber").textContent = secretNumber;
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector("#highScore").textContent = highscore;
+    }
   } else if (usersInput > secretNumber) {
-    document.querySelector("#hint").textContent = "Too high!";
-    score--;
-    document.querySelector("#score").textContent = score;
+    if (score > 1) {
+      document.querySelector("#hint").textContent = "Too high!";
+      score--;
+      document.querySelector("#score").textContent = score;
+    } else {
+      document.querySelector("#hint").textContent = "You LOST!";
+      document.querySelector("#score").textContent = 0;
+    }
   } else if (usersInput < secretNumber) {
-    document.querySelector("#hint").textContent = "Too low!";
-    score--;
-    document.querySelector("#score").textContent = score;
+    if (score > 1) {
+      document.querySelector("#hint").textContent = "Too low!";
+      score--;
+      document.querySelector("#score").textContent = score;
+    } else {
+      document.querySelector("#hint").textContent = "You LOST!";
+      document.querySelector("#score").textContent = 0;
+    }
   }
+});
+
+document.querySelector("#again").addEventListener("click", function () {
+  secretNumber = Math.floor(Math.random() * 20 + 1);
+  score = 20;
+  document.querySelector("#score").textContent = score;
+  document.querySelector("#secretNumber").textContent = "?";
+  document.querySelector("#usersInput").value = "";
+  document.querySelector("#hint").textContent = "Start guessing...";
 });
